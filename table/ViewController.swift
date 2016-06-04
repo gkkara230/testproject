a81d4ac0aadc6291679b0581b26815177a33ce2a
Govind K <gk2@govinds-imac.local>
2016-06-04 08:22:11 +0530

    Initial Commit

---
+++ table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  table
+//
+//  Created by Govind K on 04/06/16.
+//  Copyright © 2016 capgemini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
