f766cf0ca3180d9d58c4618d68323a601c5565ae
Govind K <gk2@Govinds-iMac.local>
2016-07-26 17:28:26 +0530

    First Commit

--- table/ViewController.swift
+++ table/ViewController.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var array:[String]=[]
+    var Numberofrows = 0
+    @IBOutlet weak var imageVieww: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+   
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell=UITableViewCell()
+        for item in self.array{
+            
+            print(item)
+            
+        }
+        cell.textLabel?.text=array[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        Alamofire.request(.GET, "http://demo8449518.mockable.io/zoo").responseJSON { (Response) -> Void in
+            if let JSON = Response.result.value{
+                //print(JSON["firstkey"] as! String)
+                self.array = (JSON["secondkey"] as! NSArray) as! [String]
+                self.Numberofrows = self.array.count
+                print(self.Numberofrows)
+                
+                self.tableView.reloadData()
+            }
+            
+        }
     }
+    @IBOutlet weak var imageView: UIImageView!
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           print(self.array.count)
+            return self.array.count   }
+        
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
